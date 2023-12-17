066c7ea0b3b5f13125ca9c529459605e9434f307
Elvis Musisi <117832691+elvismusisi@users.noreply.github.com>
2023-12-17 18:14:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -22,29 +22,25 @@ document.getElementById("enterBtn").addEventListener("click", function() {
     });
 
 
-    // Add this JavaScript file and link it in your HTML file
 
 document.addEventListener('DOMContentLoaded', function() {
-    // Function to update the clock
+   
     function updateClock() {
         var currentTime = new Date();
         var hours = currentTime.getHours();
         var minutes = currentTime.getMinutes();
         var seconds = currentTime.getSeconds();
 
-        // Format the time with leading zeros
         hours = (hours < 10) ? "0" + hours : hours;
         minutes = (minutes < 10) ? "0" + minutes : minutes;
         seconds = (seconds < 10) ? "0" + seconds : seconds;
 
-        // Display the time in the "clock" element
         document.getElementById('clock').innerHTML = hours + ":" + minutes + ":" + seconds;
     }
 
-    // Update the clock every second
+
     setInterval(updateClock, 1000);
 
-    // Initial clock update
     updateClock();
 });
 
